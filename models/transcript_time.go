d3f6a4d66cb2ecadebd9e36b9fb64208654109eb
Fabricio Mascarenhas <fabriciobm@gmail.com>
2024-08-27 13:36:39 -0300

    model-transcript

--- models/transcript_time.go
+++ models/transcript_time.go
@@ -5,7 +5,6 @@ type TranscriptTime struct {
     ID              string             `bson:"_id,omitempty"`
     StartTime       float64            `bson:"startTime"`
     EndTime		    float64            `bson:"endTime"`
-    UuidMaterial    string             `bson:"uuidMaterial"`
     Transcript      string             `bson:"transcript,omitempty"`
     ClassMaterialId string             `bson:"classMaterialId"`
 }
